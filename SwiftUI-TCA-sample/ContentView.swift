c8e4e61bd4764db60bbe1daa3ff147da99f5fa8d
yamshta <shymst125@gmail.com>
2022-05-01 19:31:44 +0900

    Part 2 completed

--- SwiftUI-TCA-sample/ContentView.swift
+++ SwiftUI-TCA-sample/ContentView.swift
@@ -19,23 +19,48 @@ struct AppState: Equatable {
 }
 
 enum AppAction {
-    case todoCheckboxTapped(index: Int)
-    case todoTextFieldChanged(index: Int, text: String)
+    case addButtonTapped
+    case todo(index: Int, action: TodoAction)
 }
 
 struct AppEnvironment {}
 
-let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
+    todoReducer.forEach(
+        state: \.todos,
+        action: /AppAction.todo(index:action:),
+        environment: { _ in TodoEnvironment() }
+    ),
+    Reducer { state, action, environment in
+        switch action {
+        case .addButtonTapped:
+            state.todos.insert(Todo(id: UUID()), at: 0)
+            return .none
+        case .todo:
+            return .none
+        }
+    }
+)
+
+// MARK: Todo
+
+enum TodoAction {
+    case checkboxTapped
+    case textFieldChanged(String)
+}
+
+struct TodoEnvironment {}
+
+let todoReducer = Reducer<Todo, TodoAction, TodoEnvironment> { state, action, environment in
     switch action {
-    case let .todoCheckboxTapped(index):
-        state.todos[index].isComplete.toggle()
+    case .checkboxTapped:
+        state.isComplete.toggle()
         return .none
-    case let .todoTextFieldChanged(index, text):
-        state.todos[index].description = text
+    case .textFieldChanged(let text):
+        state.description = text
         return .none
     }
 }
-.debug()
 
 struct ContentView: View {
     let store: Store<AppState, AppAction>
@@ -44,34 +69,48 @@ struct ContentView: View {
         NavigationView {
             WithViewStore(store) { viewStore in
                 List {
-                    ForEach(Array(viewStore.state.todos.enumerated()), id: \.element.id) { index, todo in
-                        HStack {
-                          Button {
-                              viewStore.send(.todoCheckboxTapped(index: index))
-                          } label: {
-                            Image(systemName: todo.isComplete ? "checkmark.square" : "square")
-                          }
-                          .buttonStyle(PlainButtonStyle())
-
-                          TextField(
-                            "Untitled todo",
-                            text: viewStore.binding(
-                                get: { $0.todos[index].description },
-                                send: { .todoTextFieldChanged(index: index, text: $0) }
-                            )
-                          )
-                        }
-                        .foregroundColor(todo.isComplete ? .gray : nil)
-                    }
+                    ForEachStore(
+                        store.scope(
+                            state: \.todos,
+                            action: AppAction.todo(index:action:)
+                        ),
+                        content: TodoView.init(store:)
+                    )
                     Text("Hello, world!")
                 }
                 .navigationTitle("Todos")
+                .navigationBarItems(trailing: Button("Add") {
+                    viewStore.send(.addButtonTapped)
+                })
             }
         }
         .navigationViewStyle(.stack) // 制約の警告を非表示にするため
     }
 }
 
+struct TodoView: View {
+    let store: Store<Todo, TodoAction>
+
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            HStack {
+                Button(action: { viewStore.send(.checkboxTapped) }) {
+                    Image(systemName: viewStore.isComplete ? "checkmark.square" : "square")
+                }
+                .buttonStyle(PlainButtonStyle())
+
+                TextField(
+                    "Untitled Todo",
+                    text: viewStore.binding(
+                        get: { $0.description },
+                        send: { .textFieldChanged($0) }
+                    )
+                )
+            }
+            .foregroundColor(viewStore.isComplete ? .gray : nil)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(
