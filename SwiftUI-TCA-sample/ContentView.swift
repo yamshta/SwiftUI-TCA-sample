499f83ab3548cf7a83ea4b0e58b611dbba7c858b
yamshta <shymst125@gmail.com>
2022-05-01 11:43:33 +0900

    Initial Commit

---
+++ SwiftUI-TCA-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-TCA-sample
+//
+//  Created by yamshta on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-TCA-sample/SwiftUI_TCA_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_sampleApp.swift
+//  SwiftUI-TCA-sample
+//
+//  Created by yamshta on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
