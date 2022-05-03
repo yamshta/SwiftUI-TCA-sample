7ed9d7c7c4f0d093bf209cba909b9cb186bf092c
yamshta <shymst125@gmail.com>
2022-05-03 14:19:34 +0900

    Add more function

--- SwiftUI-TCA-sample/TodosView.swift
+++ SwiftUI-TCA-sample/TodosView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 import ComposableArchitecture
 
+enum Filter: LocalizedStringKey, CaseIterable, Hashable {
+    case all = "All"
+    case active = "Active"
+    case completed = "Completed"
+}
+
 struct AppState: Equatable {
+    var editMode: EditMode = .inactive
+    var filter: Filter = .all
     var todos: IdentifiedArrayOf<Todo> = []
+
+    var filteredTodos: IdentifiedArrayOf<Todo> {
+        switch filter {
+        case .active: return todos.filter { !$0.isComplete }
+        case .all: return todos
+        case .completed: return todos.filter(\.isComplete)
+        }
+    }
 }
 
 enum AppAction: Equatable {
     case addTodoButtonTapped
-    case todo(id: Todo.ID, action: TodoAction)
+    case clearCompletedButtonTapped
+    case delete(IndexSet)
+    case editModeChanged(EditMode)
+    case filterPicked(Filter)
+    case move(IndexSet, Int)
     case sortCompletedTodos
+    case todo(id: Todo.ID, action: TodoAction)
 }
 
 struct AppEnvironment {
@@ -35,6 +56,40 @@ let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
             state.todos.insert(Todo(id: environment.uuid()), at: 0)
             return .none
 
+        case .clearCompletedButtonTapped:
+            state.todos.removeAll(where: \.isComplete)
+            return .none
+
+        case let .delete(indexSet):
+            state.todos.remove(atOffsets: indexSet)
+            return .none
+
+        case let .editModeChanged(editMode):
+            state.editMode = editMode
+            return .none
+
+        case let .filterPicked(filter):
+            state.filter = filter
+            return .none
+
+        case var .move(source, destination):
+            if state.filter != .all {
+                source = IndexSet(
+                    source
+                        .map { state.filteredTodos[$0] }
+                        .compactMap { state.todos.index(id: $0.id) }
+                )
+                destination =
+                state.todos.index(id: state.filteredTodos[destination].id)
+                ?? destination
+            }
+
+            state.todos.move(fromOffsets: source, toOffset: destination)
+
+            return Effect(value: .sortCompletedTodos)
+                .delay(for: .milliseconds(100), scheduler: environment.mainQueue)
+                .eraseToEffect()
+
         case .sortCompletedTodos:
             state.todos.sort { $1.isComplete && !$0.isComplete }
             return .none
@@ -53,52 +108,100 @@ let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
 struct TodosView: View {
     let store: Store<AppState, AppAction>
 
+    @ObservedObject var viewStore: ViewStore<ViewState, AppAction>
+
+    init(store: Store<AppState, AppAction>) {
+        self.store = store
+        self.viewStore = ViewStore(store.scope(state: ViewState.init(state:)))
+    }
+
+    struct ViewState: Equatable {
+        let editMode: EditMode
+        let filter: Filter
+        let isClearCompletedButtonDisabled: Bool
+
+        init(state: AppState) {
+            self.editMode = state.editMode
+            self.filter = state.filter
+            self.isClearCompletedButtonDisabled = !state.todos.contains(where: \.isComplete)
+        }
+    }
+
     var body: some View {
         NavigationView {
-            WithViewStore(store) { viewStore in
+            VStack {
+                Picker(
+                    "Filter",
+                    selection: viewStore.binding(get: \.filter, send: AppAction.filterPicked).animation()
+                ) {
+                    ForEach(Filter.allCases, id: \.self) { filter in
+                        Text(filter.rawValue).tag(filter)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+
                 List {
                     ForEachStore(
                         store.scope(
-                            state: \.todos,
+                            state: \.filteredTodos,
                             action: AppAction.todo(id:action:)
                         ),
                         content: TodoView.init(store:)
                     )
-                    Text("Hello, world!")
+                    .onDelete { self.viewStore.send(.delete($0)) }
+                    .onMove { self.viewStore.send(.move($0, $1)) }
                 }
                 .navigationTitle("Todos")
-                .navigationBarItems(trailing: Button("Add") {
-                    viewStore.send(.addTodoButtonTapped)
-                })
+                .navigationBarItems(
+                    trailing: HStack(spacing: 20) {
+                        EditButton()
+
+                        Button("Clear Completed") {
+                            viewStore.send(.clearCompletedButtonTapped, animation: .default)
+                        }
+                        .disabled(viewStore.isClearCompletedButtonDisabled)
+
+                        Button("Add Todo") {
+                            viewStore.send(.addTodoButtonTapped, animation: .default)
+                        }
+                    }
+                )
+                .environment(
+                    \.editMode,
+                     viewStore.binding(get: \.editMode, send: AppAction.editModeChanged)
+                )
             }
         }
         .navigationViewStyle(.stack) // 制約の警告を非表示にするため
     }
 }
 
+extension IdentifiedArray where ID == Todo.ID, Element == Todo {
+    static let mock: Self = [
+        Todo(
+            description: "Check Mail",
+            id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEDDEADBEEF")!,
+            isComplete: false
+        ),
+        Todo(
+            description: "Buy Milk",
+            id: UUID(uuidString: "CAFEBEEF-CAFE-BEEF-CAFE-BEEFCAFEBEEF")!,
+            isComplete: false
+        ),
+        Todo(
+            description: "Call Mom",
+            id: UUID(uuidString: "D00DCAFE-D00D-CAFE-D00D-CAFED00DCAFE")!,
+            isComplete: true
+        ),
+    ]
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         TodosView(
             store: Store(
-                initialState: AppState(
-                    todos:  [
-                        Todo(
-                            description: "Milk",
-                            id: UUID(),
-                            isComplete: false
-                        ),
-                        Todo(
-                            description: "Eggs",
-                            id: UUID(),
-                            isComplete: false
-                        ),
-                        Todo(
-                            description: "Hand Soap",
-                            id: UUID(),
-                            isComplete: true
-                        ),
-                    ]
-                ),
+                initialState: AppState(todos:  .mock),
                 reducer: appReducer,
                 environment: AppEnvironment(
                     mainQueue: DispatchQueue.main.eraseToAnyScheduler(),
--- SwiftUI-TCA-sampleTests/TodosTests.swift
+++ SwiftUI-TCA-sampleTests/TodosTests.swift
@@ -34,6 +34,32 @@ class TodosTests: XCTestCase {
         }
     }
 
+    func testEditTodo() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                )
+            ]
+        )
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(
+            .todo(id: state.todos[0].id, action: .textFieldChanged("Learn Composable Architecture"))
+        ) {
+            $0.todos[id: state.todos[0].id]?.description = "Learn Composable Architecture"
+        }
+    }
+
     func testCompletingTodo() {
         let state = AppState(
             todos: [
@@ -110,5 +136,201 @@ class TodosTests: XCTestCase {
         scheduler.advance(by: 1)
 
         store.receive(.sortCompletedTodos)
+    }
+
+    func testClearCompleted() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                    isComplete: true
+                ),
+            ]
+        )
+
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(.clearCompletedButtonTapped) {
+            $0.todos = [
+                $0.todos[0]
+            ]
+        }
+    }
+
+    func testDelete() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+                    isComplete: false
+                ),
+            ]
+        )
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(.delete([1])) {
+            $0.todos = [
+                $0.todos[0],
+                $0.todos[2],
+            ]
+        }
+    }
+
+    func testEditModeMoving() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+                    isComplete: false
+                ),
+            ]
+        )
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(.editModeChanged(.active)) {
+            $0.editMode = .active
+        }
+        store.send(.move([0], 2)) {
+            $0.todos = [
+                $0.todos[1],
+                $0.todos[0],
+                $0.todos[2],
+            ]
+        }
+        self.scheduler.advance(by: .milliseconds(100))
+        store.receive(.sortCompletedTodos)
+    }
+
+    func testEditModeMovingWithFilter() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                    isComplete: true
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000003")!,
+                    isComplete: true
+                ),
+            ]
+        )
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(.editModeChanged(.active)) {
+            $0.editMode = .active
+        }
+        store.send(.filterPicked(.completed)) {
+            $0.filter = .completed
+        }
+        store.send(.move([0], 1)) {
+            $0.todos = [
+                $0.todos[0],
+                $0.todos[2],
+                $0.todos[1],
+                $0.todos[3],
+            ]
+        }
+        self.scheduler.advance(by: .milliseconds(100))
+        store.receive(.sortCompletedTodos)
+    }
+
+    func testFilteredEdit() {
+        let state = AppState(
+            todos: [
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                    isComplete: false
+                ),
+                Todo(
+                    description: "",
+                    id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                    isComplete: true
+                ),
+            ]
+        )
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment(
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                uuid: UUID.incrementing
+            )
+        )
+
+        store.send(.filterPicked(.completed)) {
+            $0.filter = .completed
+        }
+        store.send(.todo(id: state.todos[1].id, action: .textFieldChanged("Did this already"))) {
+            $0.todos[id: state.todos[1].id]?.description = "Did this already"
+        }
     }
 }
